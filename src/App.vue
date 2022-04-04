55bc1d0396e0756fd12a396f82ec5563798a2800
Arkar <arkar.fr@gmail.com>
2022-04-04 22:53:13 +0630

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <header></header>
-  <router-view />
+  <div class="theme" :class="{ dark: themeDark }">
+    <header>
+      <Nav></Nav>
+    </header>
+    <router-view />
+  </div>
 </template>
 <script>
+import Nav from "@/components/Nav";
 export default {
   name: "App",
+  components: {
+    Nav,
+  },
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+@import "@/assets/app/scss/main";
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar navbar-dark custom-nav py-4 py-sm-3">
+    <div class="container">
+      <span class="custom-nav-title mb-0 fw-bold">Where in the world?</span>
+      <div class="toggle d-flex align-items-baseline">
+        <i class="fa-solid fa-moon me-2"></i>
+        <span class="fw-normal">Dark Mode</span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+};
+</script>
+
+<style lang="scss" scoped></style>
