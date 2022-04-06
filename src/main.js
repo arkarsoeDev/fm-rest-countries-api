c800a52df1e2e41c4b9132902123f15ce04de3a6
Arkar <arkar.fr@gmail.com>
2022-04-07 01:07:32 +0630

    add fontawesome library &  require icon in mainjs

--- src/main.js
+++ src/main.js
@@ -3,4 +3,28 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import specific icons */
+import {
+  faMoon,
+  faMagnifyingGlass,
+  faArrowLeftLong,
+  faAngleDown,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { faMoon as farMoon } from "@fortawesome/free-regular-svg-icons";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* add icons to the library */
+library.add(faMoon, farMoon, faMagnifyingGlass, faArrowLeftLong, faAngleDown);
+
+/* add font awesome icon component */
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(store)
+  .use(router)
+  .mount("#app");
