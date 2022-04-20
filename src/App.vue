arkarsoeDev/fm-rest-countries

Add Nav & toggle feature

src/App.vue
@@ -13,6 +13,11 @@ export default {
   components: {
     Nav,
   },
+  computed: {
+    themeDark() {
+      return this.$store.getters.getThemeDark;
+    },
+  },
 };
 </script>
 <style lang="scss">

arkarsoeDev/fm-rest-countries-api

src/App.vue
@@ -13,6 +13,11 @@ export default {
   components: {
     Nav,
   },
+  computed: {
+    themeDark() {
+      return this.$store.state.themeDark;
+    },
+  },
 };
 </script>
 <style lang="scss">